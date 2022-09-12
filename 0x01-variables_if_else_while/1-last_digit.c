838eb85e3e915f9a2384122ebb0ed5f1558468fc
Hanyani13 <hanyanih@gmail.com>
2022-09-12 16:44:06 -0700

    created an if statement with three optioms

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** the last digit */
+int main(void)
+{ 
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int n;
+	if n > 5;{
+		printf(and is greater than 5);
+	else if n = 0;
+		printf(and is 0);
+		else n -= 6;
+		printf(and is less than 6 and not 0);
+	}
+	return 0;
+}

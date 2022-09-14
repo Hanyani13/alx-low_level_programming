a3ba54af5b5252600d0834c8d230db5358cef3f3
Hanyani13 <hanyanih@gmail.com>
2022-09-14 16:44:06 -0700

    print last digit of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+include <stdio.h>
+
+/**
+ * main - print putchar
+ *
+ * return: always 0
+ */
+
+nt main()
+{
+	int n, sum=0,lastdigit;
+	lastdigit = n % 10;
+	putchar('lastdigit');
+	putchar('\n')
+}
+

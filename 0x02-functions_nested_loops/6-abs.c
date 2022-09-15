74d44f2495feed0c2c9e1ed725c086a7972bc706
Hanyani13 <hanyanih@gmail.com>
2022-09-15 07:11:07 -0700

    added nested functions on github

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include <stdio>
+
+/** a function that computes the absolute value of an integer
+ */
+
+void findAbsolute(int N)
+{
+	if (N < 0)
+	{
+		N = (-1) * N;
+	}
+
+	N = (-1) * N;
+}
+
+cout << ' ' << N;
+}
+
+int main(void)
+{
+	int N = -12;
+	findAbsolute(N);
+	return 0;
+}
+
+

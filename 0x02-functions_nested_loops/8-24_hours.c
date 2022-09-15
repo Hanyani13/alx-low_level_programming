e5ebe4a6335066334ada2a58d2b026fffe623042
Hanyani13 <hanyanih@gmail.com>
2022-09-14 17:11:40 -0700

    added a script that prints every minuylte of 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** 
+ * main - check the code
+ *
+ * return: Always 0.
+ */
+
+void jack_bauer(void);
+{
+	int min(00:01);
+	{
+		while ( min < 23:59; min++);
+		putchar('min');
+		putcher('\n')
+	}
+
+	return(0)
+}
+
+

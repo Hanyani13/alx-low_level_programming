129cd287b6a56bebd5d638be05cf632217dd163f
Hanyani13 <hanyanih@gmail.com>
2022-09-16 06:06:37 -0700

    added a script that checks if its a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,34 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+  c = '0';
+  if (c == _isdigit)
+    {
+      return (1);
+	}
+  else
+    {
+      return (0);
+    }
+}
+int main(void)
+{
+  int c;
+  c = '0';
+  while (c == _isdigit)
+    {
+    putchar('/n' c is a digit);
+    }
+  else
+    {
+    putchar('/n' c is not a digit);
+  }
+}

c0008bef247bf82c98c61362b2c66671cdada7d7
Hanyani13 <hanyanih@gmail.com>
2022-09-16 14:18:13 -0700

    added script that counts number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+	putchar(n);
+	++n;
+	}
+
+	putchar('\n');
+	return (0);
+}
+

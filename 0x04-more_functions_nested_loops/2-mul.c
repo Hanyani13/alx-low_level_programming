71f0b16c290f56d30e8fa4ffea1705f64662602a
Hanyani13 <hanyanih@gmail.com>
2022-09-16 10:52:56 -0700

    add a prototype that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+  int a = 7;
+  int b = 5;
+  if(a * b)
+    {
+      printf("%d\n", mul(7, b));
+    }
+  return (0);
+}
+void int (int a, int b)
+{
+  int a;
+  int b;
+  if(int a * b)
+    {
+      return;
+    }
+}
+

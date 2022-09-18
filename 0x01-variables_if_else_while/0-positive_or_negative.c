5acf5a5ac52539e497251aa837107165074c0bd1
Hanyani13 <hanyanih@gmail.com>
2022-09-17 21:46:26 -0700

    script that prints a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio>
-/* more headers goes there */
-/** a function that prints either positive or negative answer */
+/**
+ * main - script that prints a random number
+ *
+ * return: Always 0
+ */
 int main(void)
 {
 	int n;
 
-	 srand(time(0)) 
-	 n = rand() - RAND_MAX / 2;
-	 printf("value of n: %d\n", a, a++;
-	 return (0);
-	 }
+	srand(time(0)) 
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else
+	{
+		printf("%i is negative\n");
+	}
+	return (0);
+}
+
+	 

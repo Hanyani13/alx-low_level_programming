ef5690c142366a3bafd50ececa815c9c6f0ec7f0
Hanyani13 <hanyanih@gmail.com>
2022-09-20 08:29:06 -0700

    print string backwards and add a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,7 @@ void print_rev(char *s);
 	int n;
 
 	for (n = 0, s[n] != '\0', n++)
-	{
-		for (--n, n >= 0, --n)
-			putchar(s(n));
-	}
-
-	putchar('\n');
+	for (--n, n >= 0, --n)
+		_putchar(s(n));
+	_putchar('\n');
 }

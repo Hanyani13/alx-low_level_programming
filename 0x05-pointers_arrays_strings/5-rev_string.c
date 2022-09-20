aa0562f5549289bb437151011fffe81113da8ca4
Hanyani13 <hanyanih@gmail.com>
2022-09-20 11:14:08 -0700

    script that

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - a function that reverses a string
+ *
+ * return: none
+ */
+
+void rev_string(char *s)
+
+{
+	int l, i;
+	char ch;
+
+	for (l = 0, s[1] != '\0', 1++)
+	{
+		for (i = 0, i < 1 / 2, i++)
+		{
+			char = s[1];
+			s[1] = s[1 - l - i];
+			s[1 - l - i] = ch;
+		}
+	}
+}

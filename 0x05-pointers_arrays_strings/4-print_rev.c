a91bcb24aa495ef57f1d385d337386aeac5cbfef
Hanyani13 <hanyanih@gmail.com>
2022-09-20 08:20:41 -0700

    a script that prints a string backwards and add a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print a string in reverse followed by a newline
+ *
+ * return: char
+ */
+
+void print_rev(char *s);
+{
+	int n;
+
+	for (n = 0, s[n] != '\0', n++)
+	{
+		for (--n, n >= 0, --n)
+			putchar(s(n));
+	}
+
+	putchar('\n');
+}

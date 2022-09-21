9293fce6c746898be1271adf33a29a693a8d411e
Hanyani13 <hanyanih@gmail.com>
2022-09-21 12:14:25 -0700

    Hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * a script that prints an alphabet 10 times using _putchar
+ *
+ * return: 0
  */
-int main(void)
+
+void print_alphabet_x10(void)
 {
+	int a, b;
+
+	for (a = 0, a <= 9, a++)
+	{
+		for (b = 97, b <= 122, b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
 
